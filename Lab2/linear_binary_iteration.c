918854018ab3fd6fff3cefd418f084db3ab829e1
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-06-30 01:19:47 +0530

    Create linear_binary_iteration.c

---
+++ Lab2/linear_binary_iteration.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 100000
+
+int linearSearch(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+        if (arr[i] == key)
+            return i;
+    return -1;
+}
+
+int binarySearch(int arr[], int l, int r, int key)
+{
+    while (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] < key)
+            l = mid + 1;
+
+        else
+            r = mid - 1;
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int arr[SIZE], key, choice;
+    clock_t start, end;
+    int index = 0, n;
+    double time_taken;
+
+    while (choice != 3)
+    {
+
+        printf("\nChoose\n"
+               "1. Linear Search\n"
+               "2. Binary search\n"
+               "3. Exit\n");
+        scanf("%d", &choice);
+
+        if (choice == 3)
+            break;
+
+        printf("Enter value for n:");
+        scanf("%d", &n);
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = i;
+        }
+        key = arr[n - 1];
+
+        start = clock();
+        switch (choice)
+        {
+        case 1:
+            for (int i = 0; i < 1000; i++)
+            {
+                index = linearSearch(arr, n, key);
+            }
+            break;
+
+        case 2:
+            for (int i = 0; i < 1000; i++)
+            {
+                index = binarySearch(arr, 0, n - 1, key);
+            }
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+        end = clock();
+
+        time_taken = (end - start) / (double)CLOCKS_PER_SEC * 1000;
+
+        if (index == -1)
+            printf("\nElement not found!\n");
+        else
+            printf("\nElement found at position %d\n", index);
+
+        printf("Execution time is %lf\n", time_taken);
+    }
+}
